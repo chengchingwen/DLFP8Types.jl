a2dff8621b0dad6ac14c6e6b395811358d514079
Documenter.jl <documenter@juliadocs.github.io>
2024-02-09 09:06:55 +0000

    build based on ae882c1

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DLFP8","category":"page"},{"location":"#DLFP8","page":"Home","title":"DLFP8","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DLFP8.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DLFP8]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DLFP8Types","category":"page"},{"location":"#DLFP8Types","page":"Home","title":"DLFP8Types","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DLFP8Types.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DLFP8Types]","category":"page"}]
 }
